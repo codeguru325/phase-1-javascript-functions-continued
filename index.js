5c9f0f226d0808052d4e4d345de15b6b91c55f57
samuel <sammykuria300@gmail.com>
2023-09-23 13:47:27 +0300

    Quick commit

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+
+}
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+    
+}
+
+function wrapAdjective(flair = "*", ) {
+    // This inner function takes an adjective as a parameter and returns a formatted string
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  
